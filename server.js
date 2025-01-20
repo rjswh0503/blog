7e3a7c9154208a3f1f306b35f40b639c488a111b
rjswh0503 <rjswh0503@yahoo.com>
2025-01-20 19:26:24 +0900

    blog main

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const methodOverride = require('method-override')
+
+// server.js에서 css파일을 사용하기 위한 코드 
+// .css .js .jpg 파일들은 보통 static파일들 이라고 한다.
+app.use(express.static(__dirname + "/public"));
+
+
+// html 파일에 데이터를 넣고 싶으면 .ejs 파일로 만들면 가능.
+app.set("view engine", "ejs");
+// 요청.body 사용할려면 필수
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
+
 
 
 app.listen(port, () => {
@@ -16,5 +31,9 @@ app.listen(port, () => {
 
 //blog main 페이지 엔드포인트
 app.get('/', (req,res) => {
-    res.json({message: '메인페이지 입니다...'})
+    res.render('main/main.ejs')
 })
+
+
+
+
